60664e819bac7ad3ce70c6db814ec6255da47fc3
Phillip Williams <fillip1984@gmail.com>
2026-01-01 17:36:11 -0500

    added menu to select activity

--- src/components/floating.tsx
+++ src/components/floating.tsx
@@ -1,9 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import { BsPalette } from "react-icons/bs";
 import { ModeToggle } from "./theme-toggle";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function floating() {
   return (
     <div className="absolute right-4 bottom-4">
-      <ModeToggle />
+      <div className="flex flex-col gap-3">
+        <ActivitySelector />
+        <ModeToggle />
+      </div>
     </div>
   );
 }
+
+const ActivitySelector = () => {
+  const [selectedActivity, setSelectedActivity] = useState("Free");
+  const activities = [
+    "Chores",
+    "Leisure",
+    "Running",
+    "Sleeping",
+    "Stretching",
+    "Weight Lifting",
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <BsPalette className="" />
+          <span className="sr-only">Select Activity</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuRadioGroup
+          value={selectedActivity}
+          onValueChange={setSelectedActivity}
+        >
+          <DropdownMenuRadioItem
+            key="Free"
+            value="Free"
+            onClick={() => setSelectedActivity("Free")}
+          >
+            Free
+          </DropdownMenuRadioItem>
+          <DropdownMenuSeparator />
+          {activities.map((activity) => (
+            <DropdownMenuRadioItem
+              key={activity}
+              value={activity}
+              onClick={() => setSelectedActivity(activity)}
+            >
+              {activity}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
