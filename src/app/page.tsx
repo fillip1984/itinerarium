776d23259d22b18aee8899aa05cba0d01064e6f2
Phillip Williams <fillip1984@gmail.com>
2026-01-02 12:29:35 -0500

    cleaned up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -339,7 +339,7 @@ const DayChart = ({ timeslots }: { timeslots: TimeslotType[] }) => {
   >([]);
 
   useEffect(() => {
-    // console.log("Timeslots updated:", timeslots);
+    // sum and sort activities by hours into chart data format
     const activityGroups = timeslots.reduce(
       (acc, ts) => {
         const activityName = ts.activity?.name ?? "Free";
@@ -355,20 +355,16 @@ const DayChart = ({ timeslots }: { timeslots: TimeslotType[] }) => {
       },
       {} as Record<string, { activity: string; hours: number; fill: string }>,
     );
-    setChartData(Object.values(activityGroups));
-    console.log("Activity Groups:", activityGroups);
-
-    // const chartData = Object.values(activityGroups);
-    // console.log("Chart Data:", chartData);
+    setChartData(
+      Object.values(activityGroups).sort((a, b) => {
+        if (a.activity === "Free") return 1;
+        if (b.activity === "Free") return -1;
+        return b.hours - a.hours;
+      }),
+    );
   }, [timeslots]);
 
-  // const chartData = [
-  //   { activity: "Sleep", hours: 8, fill: "var(--color-Sleep)" },
-  //   { activity: "Work", hours: 8, fill: "var(--color-Work)" },
-  //   { activity: "Free", hours: 4, fill: "var(--color-Free)" },
-  //   { activity: "Other", hours: 4, fill: "var(--color-Other)" },
-  // ];
-
+  // TODO: make this dynamic based on activities used
   const chartConfig = {
     hours: {
       label: "Hours",
@@ -407,46 +403,8 @@ const DayChart = ({ timeslots }: { timeslots: TimeslotType[] }) => {
     },
   } satisfies ChartConfig;
 
-  // const chartData = [
-  //   { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  //   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  //   { browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
-  //   { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  //   { browser: "other", visitors: 90, fill: "var(--color-other)" },
-  // ];
-
-  // const chartConfig = {
-  //   visitors: {
-  //     label: "Visitors",
-  //   },
-  //   chrome: {
-  //     label: "Chrome",
-  //     color: "var(--chart-1)",
-  //   },
-  //   safari: {
-  //     label: "Safari",
-  //     color: "var(--chart-2)",
-  //   },
-  //   firefox: {
-  //     label: "Firefox",
-  //     color: "var(--chart-3)",
-  //   },
-  //   edge: {
-  //     label: "Edge",
-  //     color: "var(--chart-4)",
-  //   },
-  //   other: {
-  //     label: "Other",
-  //     color: "var(--chart-5)",
-  //   },
-  // } satisfies ChartConfig;
-
   return (
     <Card className="flex flex-col">
-      {/* <CardHeader className="items-center pb-0"> */}
-      {/* <CardTitle>Pie Chart - Label List</CardTitle> */}
-      {/* <CardDescription>January - June 2024</CardDescription> */}
-      {/* </CardHeader> */}
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
@@ -470,14 +428,6 @@ const DayChart = ({ timeslots }: { timeslots: TimeslotType[] }) => {
           </PieChart>
         </ChartContainer>
       </CardContent>
-      {/* <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 leading-none font-medium">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="text-muted-foreground leading-none">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter> */}
     </Card>
   );
 };
